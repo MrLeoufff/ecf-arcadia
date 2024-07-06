60a53d8533e0749cd1cd97b6088edf27120ff299
MrLeoufff <mrleoufff@gmail.com>
2024-07-06 11:06:42 +0200

    add gestion des horaires

---
+++ src/Controller/ScheduleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Schedule;
+use App\Form\ScheduleType;
+use App\Repository\ScheduleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin/schedule', name: 'app_schedule_')]
+#[IsGranted('ROLE_ADMIN')]
+class ScheduleController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(ScheduleRepository $scheduleRepository): Response
+    {
+        return $this->render('schedule/index.html.twig', [
+            'schedules' => $scheduleRepository->findAll()
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $schedule = new Schedule();
+        $form = $this->createForm(ScheduleType::class, $schedule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($schedule);
+            $em->flush();
+
+            return $this->redirectToRoute('app_schedule_index');
+        }
+
+        return $this->render('schedule/new.html.twig', [
+            'schedule' => $schedule,
+            'form' =>$form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Schedule $schedule, EntityManagerInterface  $em): Response
+    {
+        $form = $this->createForm(ScheduleType::class, $schedule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_schedule_index');
+        }
+
+        return $this->render('schedule/edit.html.twig', [
+            'schedule' => $schedule,
+            'form' =>$form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Schedule $schedule, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$schedule->getId(), $request->request->get('_token'))) {
+            $em->remove($schedule);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_schedule_index');
+    }
+
+}
---
+++ src/Form/ScheduleType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Schedule;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ScheduleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('day', ChoiceType::class, [
+                'choices' => [
+                    'Lundi' => 'Lundi',
+                    'Mardi' => 'Mardi',
+                    'Mercredi' => 'Mercredi',
+                    'Jeudi' => 'Jeudi',
+                    'Vendredi' => 'Vendredi',
+                    'Samedi' => 'Samedi',
+                    'Dimanche' => 'Dimanche',
+                ]
+            ])
+            ->add('opening_time', TimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('closing_time', TimeType::class, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Schedule::class,
+        ]);
+    }
+}
